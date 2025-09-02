26e03c1811578d61ecc0104018c32733524713f3
Evandro Cunha <evandrocunhajunior@gmail.com>
2025-09-01 21:50:59 -0300

    add server config

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ message: "API rodando com TypeScript 🚀" });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export default app;
